8b6d1850405ee00a57716fe3cc6e6fb363268de1
Juhyeon <qlswngus11@naver.com>
2024-11-16 20:09:01 +0900

    코드 수정

--- js/common.js
+++ js/common.js
@@ -8,29 +8,36 @@ $(function () {
         }
     }
 
-    lightCheck();
-
     $('.button-cover').click(function () {
         lightCheck();
     });
 
-    // checked 확인 후 class 처리 js
-    $('#lightCheck').change(function () {
-        if ($(this).is(':checked')) {
-            localStorage.setItem('lightStatus', 'checked');
-        } else {
-            localStorage.setItem('lightStatus', 'unchecked');
-        }
-    });
-
     let lightStatus = localStorage.getItem('lightStatus');
 
+    // 초기 상태 설정
     if (lightStatus === 'checked') {
         $('#lightCheck').prop('checked', true);
         $('body').addClass('active');
     } else {
         $('#lightCheck').prop('checked', false);
         $('body').removeClass('active');
+    }
+
+    // 체크박스 상태 변경 시 이벤트 처리
+    $('#lightCheck').change(function () {
+        const isChecked = $(this).is(':checked');
+        updateLightStatus(isChecked);
+    });
+
+    // 상태 업데이트 함수
+    function updateLightStatus(isChecked) {
+        if (isChecked) {
+            localStorage.setItem('lightStatus', 'checked');
+            $('body').addClass('active');
+        } else {
+            localStorage.setItem('lightStatus', 'unchecked');
+            $('body').removeClass('active');
+        }
     }
 
     AOS.init();
